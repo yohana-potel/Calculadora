dff523fa0ecf0cdfd4902b1fa69312455c1e8e67
yohana potel <cpoteljunot@hotmail.com>
2023-06-09 11:11:52 -0300

    Se añadio contenido en archivo javascript

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const displayValorAnterior = document.getElementById('valorAnterior');
+const displayValorActual = document.getElementById('valorActual');
+
+
+// funciones  de las operaciones de la calculadora.
+ function suma(a,b){
+   return a+b;
+ };
+
+ function resta (a,b){
+   return a-b;
+ };
+
+ return multiplicacion (a,b){
+   return a*b;
+
+ };
+
+ function divicion(a,b){
+   return a/b;
+ };
